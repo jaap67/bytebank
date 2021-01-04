15dc840f8c90f4ebabb5bf3bb523d6b93f9698f8
José <ze_harlan@me.com>
2021-01-04 17:03:59 -0300

    Utilizando estrutura de repetição

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,29 +1,46 @@
 fun main() {
     println("Bem-vindo ao Bytebank")
-    val titular: String = "Alex"
-    val numeroConta: Int = 1000
-    var saldo: Double = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
-    saldo -= 1000
 
-    println("titular $titular")
-    println("numero $numeroConta")
-    println("saldo $saldo")
+    var i = 1
+    while (i <= 5){
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo: Double = i + 10.0
 
-//    if (saldo > 0) {
-//        println("conta é positiva")
-//    } else if (saldo == 0.0) {
-//        println("conta é neutra")
-//    } else {
-//        println("conta é negativa")
+        println("titular $titular")
+        println("numero $numeroConta")
+        println("saldo $saldo")
+        println()
+        i += 1
+    }
+//    for (i in 5 downTo 1) {
+//        if (i == 4) {
+//            continue
+//        }
+//        val titular: String = "Alex $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo: Double = i + 10.0
+//
+//        println("titular $titular")
+//        println("numero $numeroConta")
+//        println("saldo $saldo")
+//        println()
 //    }
+//    testaCondicoes(saldo)
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
 
     when {
         saldo > 0 -> println("conta é positiva")
         saldo == 0.0 -> println("conta é neutra")
         else -> println("conta é negativa")
     }
-
-    // Duas expressões que são equivalentes, if e when
 }
