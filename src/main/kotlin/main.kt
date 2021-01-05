822c522850d32c697cfb0a704fe2be2fdf537b56
José <ze_harlan@me.com>
2021-01-05 11:54:31 -0300

    Testando ifs, when e @loop

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -13,20 +13,28 @@ fun main() {
         println()
         i += 1
     }
-//    for (i in 5 downTo 1) {
-//        if (i == 4) {
-//            continue
-//        }
-//        val titular: String = "Alex $i"
-//        val numeroConta: Int = 1000 + i
-//        var saldo: Double = i + 10.0
-//
-//        println("titular $titular")
-//        println("numero $numeroConta")
-//        println("saldo $saldo")
-//        println()
-//    }
-//    testaCondicoes(saldo)
+
+    loop@ for (i in 1..100) {
+        println("i $i")
+        for (j in 1..100) {
+            println("j $j")
+            if (j == 5) break@loop
+        }
+    }
+    for (i in 5 downTo 1) {
+        if (i == 4) {
+            continue
+        }
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo: Double = i + 10.0
+
+        println("titular $titular")
+        println("numero $numeroConta")
+        println("saldo $saldo")
+        println()
+    }
+    testaCondicoes(saldo)
 }
 
 fun testaCondicoes(saldo: Double) {
