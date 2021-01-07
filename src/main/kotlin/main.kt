d77fac563c378f96b98261ac9048eb64a28c73e1
José <ze_harlan@me.com>
2021-01-07 10:34:44 -0300

    05 - Utilizando properties

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,67 +4,70 @@ fun main() {
     val contaAlex = Conta()
     contaAlex.titular = "Alex"
     contaAlex.numero = 1000
-    contaAlex.setSaldo(-200.0)
+    contaAlex.deposita(-200.0)
 
     println("=================")
     println(contaAlex.titular)
     println(contaAlex.numero)
-    println(contaAlex.getSaldo())
+    println(contaAlex.saldo)
     println("=================")
     println()
 
     val contaFran = Conta()
     contaFran.titular = "Fran"
     contaFran.numero = 1001
-    contaFran.setSaldo(300.0)
+    contaFran.deposita(300.0)
 
     println("=================")
     println(contaFran.titular)
     println(contaFran.numero)
-    println(contaFran.getSaldo())
+    println(contaFran.saldo)
     println("=================")
-//
-//    println("depositando na conta do Alex")
-//    contaAlex.deposita(50.0)
-//    println(contaAlex.saldo)
-//
-//    println("sacando da conta do Alex")
-//    contaAlex.saca(250.0)
-//    println(contaAlex.saldo)
-//
-//    println("depositando na conta da Fran")
-//    contaFran.deposita(70.0)
-//    println(contaFran.saldo)
-//
-//    println("sacando da conta da Fran")
-//    contaFran.saca(100.0)
-//    println(contaFran.saldo)
-//
-//    println("saque em excesso na conta do Alex")
-//    contaAlex.saca(100.0)
-//    println(contaAlex.saldo)
-//
-//    println("saque em excesso na conta da Fran")
-//    contaFran.saca(500.0)
-//    println(contaFran.saldo)
-//
-//    println("Transferência da conta da Fran para o Alex")
-//    if (contaFran.transfere(300.0, contaAlex)) {
-//        println("Transferência sucedida")
-//    } else {
-//        println("Falha na tranferência")
-//    }
-//    println("saldo alex ${contaAlex.saldo}")
-//    println("saldo fran  ${contaFran.saldo}")
+
+    println("depositando na conta do Alex")
+    contaAlex.deposita(50.0)
+    println(contaAlex.saldo)
+
+    println("sacando da conta do Alex")
+    contaAlex.saca(250.0)
+    println(contaAlex.saldo)
+
+    println("depositando na conta da Fran")
+    contaFran.deposita(70.0)
+    println(contaFran.saldo)
+
+    println("sacando da conta da Fran")
+    contaFran.saca(100.0)
+    println(contaFran.saldo)
+
+    println("saque em excesso na conta do Alex")
+    contaAlex.saca(100.0)
+    println(contaAlex.saldo)
+
+    println("saque em excesso na conta da Fran")
+    contaFran.saca(500.0)
+    println(contaFran.saldo)
+
+    println("Transferência da conta da Fran para o Alex")
+    if (contaFran.transfere(300.0, contaAlex)) {
+        println("Transferência sucedida")
+    } else {
+        println("Falha na tranferência")
+    }
+    println("saldo alex ${contaAlex.saldo}")
+    println("saldo fran  ${contaFran.saldo}")
 }
 
 class Conta {
     var titular = ""
     var numero = 0
-    private var saldo = 0.0
+    var saldo = 0.0
+        private set
 
     fun deposita(valor: Double) {
-        this.saldo += valor
+        if (valor > 0) {
+            this.saldo += valor
+        }
     }
 
     fun saca(valor: Double) {
@@ -81,17 +84,6 @@ class Conta {
         }
         return false
     }
-
-    fun getSaldo(): Double {
-        return saldo
-    }
-
-    fun setSaldo(valor: Double) {
-        if (valor > 0){
-            saldo = valor
-        }
-    }
-
 }
 
 fun testaCopiasEReferencias() {
