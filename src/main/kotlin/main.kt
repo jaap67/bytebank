95990da86372d519e066602e6817c9fe6d0c06c5
José <ze_harlan@me.com>
2021-01-08 10:38:15 -0300

    Revert "Criando classes para cada funcionário"
    This reverts commit 2914e8cd316242ec2279fd278ba561af88de9aee.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,7 +4,8 @@ fun main() {
     val alex = Funcionario(
         nome = "Alex",
         cpf = "111.111.111-11",
-        salario = 1000.0
+        salario = 1000.0,
+        tipo = 1
     )
 
     println("nome ${alex.nome}")
@@ -12,19 +13,4 @@ fun main() {
     println("salario ${alex.salario}")
     println("bonificação ${alex.bonificacao()}")
 
-    val fran = Gerente(
-        nome = "Fran",
-        cpf = "222.222.222-22",
-        salario = 2000.0,
-        senha = 1234
-    )
-    println("nome ${fran.nome}")
-    println("cpf ${fran.cpf}")
-    println("salario ${fran.salario}")
-    println("bonificação ${fran.bonificacao()}")
-    if (fran.autentica(12344)){
-        println("autenticou com sucesso")
-    } else {
-        println("falha na autenticação")
-    }
 }
