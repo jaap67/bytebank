266c6c58c99c353b371b18c243ae4a6af5e2844a
José <ze_harlan@me.com>
2021-01-11 17:00:35 -0300

    kotlin2 - 05 - Implementando o funcionário admin

--- src/main/kotlin/Gerente.kt
+++ src/main/kotlin/Gerente.kt
@@ -2,11 +2,12 @@ class Gerente(
     nome: String,
     cpf: String,
     salario: Double,
-    val senha: Int
-) : Funcionario(
+    senha: Int
+) : FuncionarioAdmin(
     nome = nome,
     cpf = cpf,
-    salario = salario
+    salario = salario,
+    senha = senha
 ) {
 
 
@@ -14,11 +15,4 @@ class Gerente(
         get() {
             return salario
         }
-
-    fun autentica(senha: Int): Boolean {
-        if (this.senha == senha) {
-            return true
-        }
-        return false
-    }
 }
