cdc8fbbd3218af8a1ddaeff50cada51f304531d7
José <ze_harlan@me.com>
2021-01-11 10:57:08 -0300

    kotlin2 - 04 - Utilizando abstrações

---
+++ src/main/kotlin/testaContasDiferentes.kt
@@ -0,0 +1,26 @@
+fun testaContasDiferentes() {
+    val contaCorrente = ContaCorrente(titular = "Alex", numero = 1000)
+    val contaPoupanca = ContaPoupanca(titular = "Fran", numero = 1001)
+
+    contaCorrente.deposita(1000.0)
+    contaPoupanca.deposita(1000.0)
+
+    println("saldo corrente: ${contaCorrente.saldo}")
+    println("saldo poupanca: ${contaPoupanca.saldo}")
+
+    contaCorrente.saca(100.0)
+    contaPoupanca.saca(100.0)
+
+    println("saldo corrente após saque: ${contaCorrente.saldo}")
+    println("saldo poupanca após saque: ${contaPoupanca.saldo}")
+
+    contaCorrente.transfere(100.0, contaPoupanca)
+
+    println("saldo corrente após transferir para poupança: ${contaCorrente.saldo}")
+    println("saldo poupanca após receber transferência: ${contaPoupanca.saldo}")
+
+    contaPoupanca.transfere(100.0, contaCorrente)
+
+    println("saldo poupança após transferir para corrente: ${contaPoupanca.saldo}")
+    println("saldo corrente após receber transferência: ${contaCorrente.saldo}")
+}
