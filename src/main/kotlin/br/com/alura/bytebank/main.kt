429505de3d22c01cc1e6deb5807d15e7652e8f1d
José <ze_harlan@me.com>
2021-01-14 09:35:06 -0300

    kotlin3 - 03 - utilizando variáveis globais

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,9 +1,19 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.ContaCorrente
+import br.com.alura.bytebank.modelo.ContaPoupanca
+import br.com.alura.bytebank.modelo.totalContas
 import br.com.alura.bytebank.teste.testaContasDiferentes
 
 fun main() {
     println("Bem-vindo ao Bytebank")
 
+    val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
+    val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000)
+    val contaCorrente = ContaCorrente(titular = alex, numero = 1001)
+
     testaContasDiferentes()
+
+    println("Total de contas: $totalContas")
 }
