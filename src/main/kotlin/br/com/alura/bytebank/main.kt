30646474d632a2c2c17ae30eb3f51fe988c8b737
José <ze_harlan@me.com>
2021-01-14 16:40:09 -0300

    kotlin3 - 04 - Conhecendo o Any

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,31 +1,39 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.modelo.*
-import br.com.alura.bytebank.teste.testaContasDiferentes
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    println("Bem-vindo ao Bytebank")
 
-    val fran = object : Autenticavel{
-        val nome: String = "Fran"
-        val cpf: String = "111.111.111-11"
-        val senha: Int = 1000
+    println()
+    println(1)
+    println(1.0)
+    println(true)
+    println("teste")
 
-        override fun autentica(senha: Int) = this.senha == senha
-    }
+    val endereco = Endereco()
+    println(endereco)
+    println(Cliente("ze", "", endereco, 1))
 
-    val sistemaInterno = SistemaInterno()
-    sistemaInterno.entra(fran, 1000)
-
-    println("nome do cliente ${fran.nome}")
-
-
-
-    val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
-    val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000)
-    val contaCorrente = ContaCorrente(titular = alex, numero = 1001)
-
-    testaContasDiferentes()
-
-    println("Total de contas: ${Conta.total}")
+    /**
+     * Todas as classes no kotlin herdam de Any.
+     */
+    imprime()
+    imprime(1)
+    imprime(1.0)
+    imprime(endereco)
 }
+
+fun imprime(){
+
+}
+
+fun imprime(valor: Int) {
+
+}
+
+fun imprime(valor: Double) {
+
+}
+
+fun imprime(endereco: Endereco){}
