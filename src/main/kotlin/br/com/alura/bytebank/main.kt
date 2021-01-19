c585dbae9b7b00937d6d7ed9f3b4d06952926627
José <ze_harlan@me.com>
2021-01-19 11:42:15 -0300

    kotlin4 - 03 - Criando exceptions

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,5 +1,6 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.exception.SaldoInsuficienteException
 import br.com.alura.bytebank.modelo.Endereco
 import java.lang.ClassCastException
 
@@ -13,9 +14,9 @@ fun funcao1() {
     println("início funcao1")
     try {
         funcao2()
-    } catch (e: ClassCastException) {
+    } catch (e: SaldoInsuficienteException) {
         e.printStackTrace()
-        println("ClassCastException foi pegada")
+        println("SaldoInsuficienteException foi pegada")
     }
     println("fim funcao1")
 }
@@ -24,8 +25,7 @@ fun funcao2() {
     println("início funcao2")
     for (i in 1..5) {
         println(i)
-        val endereco = Any()
-        throw ClassCastException()
+        throw SaldoInsuficienteException()
     }
     println("fim funcao2")
 }
