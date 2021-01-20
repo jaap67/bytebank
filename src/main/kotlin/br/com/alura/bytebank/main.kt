8f746b6eff61f37a58d49783396f2b67d6d96917
José <ze_harlan@me.com>
2021-01-20 18:44:44 -0300

    kotlin4 - 05 - Outros recursos do null safety

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -4,12 +4,28 @@ import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
 
-    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro")
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro", complemento = "prédio")
 //    enderecoNulo?.let {endereco ->
 //        println(endereco.logradouro.length)
 //    }
     enderecoNulo?.let {
         println(it.logradouro.length)
+        val tamanhoComplemento: Int = it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser vazio")
+        println(tamanhoComplemento)
     }
+
+//    val x = foo(enderecoNulo)
+//    println(x)
+
+    teste("")
 }
 
+fun foo(endereco: Endereco?): String? {
+//    val tamanho = endereco?.complemento ?: return null
+    return endereco?.complemento ?: throw IllegalArgumentException("name expected")
+}
+
+fun teste(valor: Any) {
+    val numero: Int? = valor as? Int
+    println(numero)
+}
