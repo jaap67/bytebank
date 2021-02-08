bb3507307b1cfc1477236791a7deffc94d6c15d7
José <ze_harlan@me.com>
2021-02-08 13:55:29 -0300

    kotlin functional - 02 - Tipo função com parâmetros e retorno

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -2,37 +2,35 @@ package br.com.alura.bytebank
 
 fun main() {
 
-//    testaTipoFuncaoReferencia()
-//    testaTipoFuncaoClasse()
-    val minhaFuncaoLambda: () -> Unit = {
-        println("Executa como lambda")
-    }
-    println(minhaFuncaoLambda) // sem executar
-    println(minhaFuncaoLambda()) // executando a funcão
-
-    val minhaFuncaoAnonima: () -> Unit = fun() {
-        println("Executa como anônima")
-    }
-    println(minhaFuncaoAnonima)
-    println(minhaFuncaoAnonima())
+    testaTipoFuncaoReferencia()
+    testaTipoFuncaoClasse()
+//    val minhaFuncaoLambda: () -> Unit = {
+//        println("Executa como lambda")
+//    }
+//    println(minhaFuncaoLambda) // sem executar
+//    println(minhaFuncaoLambda()) // executando a funcão
+//
+//    val minhaFuncaoAnonima: () -> Unit = fun() {
+//        println("Executa como anônima")
+//    }
+//    println(minhaFuncaoAnonima)
+//    println(minhaFuncaoAnonima())
 }
 
 fun testaTipoFuncaoClasse() {
-    val minhaFuncaoClasse: () -> Unit = Teste()
-    println(minhaFuncaoClasse())
+    val minhaFuncaoClasse: (Int, Int) -> Int= Soma()
+    println(minhaFuncaoClasse(10, 10))
 }
 
 fun testaTipoFuncaoReferencia() {
-    val minhaFuncao: () -> Unit = ::teste
-    println(minhaFuncao())
+    val minhaFuncao: (Int, Int) -> Int = ::soma
+    println(minhaFuncao(5, 10))
 }
 
-fun teste() {
-    println("executa teste")
+fun soma(a: Int, b: Int) : Int {
+    return a + b
 }
 
-class Teste : () -> Unit {
-    override fun invoke() {
-        println("executa invoke do Teste")
-    }
+class Soma : (Int, Int) -> Int {
+    override fun invoke(a: Int, b: Int): Int = a + b
 }
