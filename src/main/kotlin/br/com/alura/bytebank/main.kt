ec16908fe983f2fcd8a1920843fb10d61efdcde8
José <ze_harlan@me.com>
2021-02-08 11:11:38 -0300

    kotlin functional - 01 - Expressão lambda e função anônima

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -2,10 +2,29 @@ package br.com.alura.bytebank
 
 fun main() {
 
+//    testaTipoFuncaoReferencia()
+//    testaTipoFuncaoClasse()
+    val minhaFuncaoLambda: () -> Unit = {
+        println("Executa como lambda")
+    }
+    println(minhaFuncaoLambda) // sem executar
+    println(minhaFuncaoLambda()) // executando a funcão
+
+    val minhaFuncaoAnonima: () -> Unit = fun() {
+        println("Executa como anônima")
+    }
+    println(minhaFuncaoAnonima)
+    println(minhaFuncaoAnonima())
+}
+
+fun testaTipoFuncaoClasse() {
+    val minhaFuncaoClasse: () -> Unit = Teste()
+    println(minhaFuncaoClasse())
+}
+
+fun testaTipoFuncaoReferencia() {
     val minhaFuncao: () -> Unit = ::teste
     println(minhaFuncao())
-    val minhaFuncaoClasses: () -> Unit = Teste()
-    println(minhaFuncaoClasses())
 }
 
 fun teste() {
