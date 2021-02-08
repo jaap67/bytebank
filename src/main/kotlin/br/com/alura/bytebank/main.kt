4c40377066fa2468d58315893c380b94c18900cc
Arlan Linux <marinhomoraisster@gmail.com>
2021-02-08 10:45:43 -0300

    kotlin functional - 01 - Voltando código para poder extrair

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -2,28 +2,17 @@ package br.com.alura.bytebank
 
 fun main() {
 
-//    val teste = Teste()
-//    println(teste)
-//    teste()
-
-    val teste = Teste()
-    teste(10)
-    val testeFuncao: () -> Unit = Teste()
-    testeFuncao()
+    val minhaFuncao: () -> Unit = ::teste
+    println(minhaFuncao())
+    val minhaFuncaoClasses: () -> Unit = Teste()
+    println(minhaFuncaoClasses())
 }
 
-//class Teste {
-//    operator fun invoke() {
-//        println("executa invoke do Teste")
-//    }
-//}
+fun teste() {
+    println("executa teste")
+}
 
 class Teste : () -> Unit {
-
-    operator fun invoke(valor: Int){
-        println(valor)
-    }
-
     override fun invoke() {
         println("executa invoke do Teste")
     }
